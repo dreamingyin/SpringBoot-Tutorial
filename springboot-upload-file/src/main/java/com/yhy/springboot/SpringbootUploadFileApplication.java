94d7aaf920279406f0ae8edd0a7f3580d18bd886
2932895862@qq.com <haoyeyin@gmail.com>
2020-03-15 10:39:58 +0800

    第十二篇：上传文件

---
+++ springboot-upload-file/src/main/java/com/yhy/springboot/SpringbootUploadFileApplication.java
@@ -0,0 +1,27 @@
+package com.yhy.springboot;
+
+import com.yhy.springboot.storage.StorageProperties;
+import com.yhy.springboot.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class SpringbootUploadFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUploadFileApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
+}
---
+++ springboot-upload-file/src/main/java/com/yhy/springboot/storage/StorageException.java
@@ -0,0 +1,15 @@
+package com.yhy.springboot.storage;
+
+/**
+ * @author Steven
+ */
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
